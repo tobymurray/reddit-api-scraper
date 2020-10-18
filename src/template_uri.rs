cb0ba0b3b61395c5d8764b80bda054df83196f8f
Toby Murray <toby.murray@protonmail.com>
2020-10-18 17:26:41 -0400

    Implement display for TemplateUri

--- src/template_uri.rs
+++ src/template_uri.rs
@@ -1,7 +1,14 @@
 use std::collections::HashMap;
+use std::fmt;
 
 pub struct TemplateUri {
   pub template: String,
   pub parameters: HashMap<String, String>,
-  pub request_fields: HashMap<String, String>,
+  pub request_fields: HashMap<String, String>,  
+}
+
+impl fmt::Display for TemplateUri {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}, {:?}", self.template, self.parameters)
+  }
 }
